5727ddaa40a1fbb6a6bee244b34b52f095eb82c4
krishpranav <krisna.pranav@gmail.com>
2022-06-09 11:51:51 +0530

    hitting the basic server

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/krishpranav/golang-management/middleware"
+	"github.com/krishpranav/golang-management/routers"
 )
 
+// server is running[now we can able to create users]
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -15,4 +18,8 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
+	routers.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.Run(":" + port)
 }
